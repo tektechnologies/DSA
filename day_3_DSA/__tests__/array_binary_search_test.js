2c15f6f43ce24bccb5233fc469c8e40917dfaf7d
tektechnologies <info@tektechnologies.com>
2018-08-15 14:34:15 -0500

    this add first test

--- day_3_DSA/__tests__/array_binary_search_test.js
+++ day_3_DSA/__tests__/array_binary_search_test.js
@@ -3,26 +3,26 @@
 const arrayInsert = require('../array_binary_search');
 
 describe('arrayShift', () => {
-  it('', () => {
-        expect().toEqual();
+  it('array_binary_search will search array to find index of value', () => {
+        expect([1,2,3,4,5], 5).toEqual([4]);
   });
 
-  it('', () => {
-        expect().toEqual();
-  });
+//   it('', () => {
+//         expect().toEqual();
+//   });
 
-  it('', () => {
-        expect().toEqual();
-  });
+//   it('', () => {
+//         expect().toEqual();
+//   });
 
   
 
-  it('', () => {
-        expect().toBe();
-  });
+//   it('', () => {
+//         expect().toBe();
+//   });
 
-  it('returns given array if value is missing', () => {
+//   it('returns given array if value is missing', () => {
        
-        expect().toBe();
-  });
+//         expect().toBe();
+//   });
 });
--- day_3_DSA/array_binary_search.js
+++ day_3_DSA/array_binary_search.js
@@ -1,11 +1,19 @@
 'use strict';
 
-function binarySearch() {
-  
+function binarySearch(array, value) {
+  let splitArray;
+  let minVal = 0;
+  let maxVal = array.length - 1;
+
+  while(minVal < maxVal){
+    splitArray = Math.ceil((minVal+maxVal)/2);
+    console.log(splitArray);
+
+  };
 
 
 
-  
+
   console.log();
   
   
