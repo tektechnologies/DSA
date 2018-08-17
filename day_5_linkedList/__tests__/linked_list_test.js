6b4cb9672b8fc41099d92a237d4ee141ea2efc59
tektechnologies <info@tektechnologies.com>
2018-08-17 14:45:06 -0500

    adds initial setup of files

---
+++ day_5_linkedList/__tests__/linked_list_test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const linkedList = require('../linked_list');
+
+describe('arrayShift', () => {
+  it('', () => {
+    expect().toEqual();
+  });
+
+  it('', () => {
+    expect().toEqual();
+  });
+
+  it('', () => {
+    expect().toEqual();
+  });
+
+  
+
+  it('', () => {
+    expect().toBe();
+  });
+
+  it('returns given array if value is missing', () => {
+       
+    expect().toBe();
+  });
+});
---
+++ day_5_linkedList/linked_list.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function linkedList() {
+  
+
+  console.log();
+  
+  
+
+}
+module.exports = linkedList;
+
+
+//console.log(linkedList(arrayOne));
+//console.log(linkedList(arrayTwo));
+//console.log(linkedList(arrayThree));
