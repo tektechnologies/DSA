a727c34a4134cbafc39dac07a004258e55f47f10
tektechnologies <info@tektechnologies.com>
2018-08-25 17:25:32 -0500

    add files for stack

--- STACK and QUEUE/lib/stack/shift-unshift.js
+++ STACK and QUEUE/lib/stack/shift-unshift.js
@@ -1,2 +1,28 @@
 'use strict';
-module.exports = require('./push-pop');
+
+class ShiftUnshiftStack {
+  constructor() {
+    this.count = 0;
+    this.values = [];
+  }
+
+  push(value) {
+    this.values.unshift(value);
+    this.count++;
+  }
+
+  peek() {
+    return this.values[0];
+  }
+
+  pop() {
+    this.count--;
+    return this.values.shift();
+  }
+
+  toArray() {
+    return this.values;
+  }
+}
+
+module.exports = ShiftUnshiftStack;
