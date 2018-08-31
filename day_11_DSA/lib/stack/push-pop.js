93a69871548e9f5b33452fbaf820a931144efc99
tektechnologies <info@tektechnologies.com>
2018-08-30 20:08:51 -0500

    add push

--- day_11_DSA/lib/stack/push-pop.js
+++ day_11_DSA/lib/stack/push-pop.js
@@ -1,34 +1,40 @@
-np'use strict';
+'use strict';
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+
+
+
 
 class PushPopStack {
   constructor() {
+    this.head = null;
     this.count = 0;
-    this.values = [];
   }
 
   push(value) {
-    this.values.push(value);
+    //creat new node
+    let newHead = new Node(value);
+    //make .next of new head this.head,
+    newHead.next = this.head;
+    //now make the new head this.head
+    this.head = newHead;
     this.count++;
   }
 
-  peek() {
-    return this.values[this.count - 1];
-  }
+ 
 
   pop() {
     this.count--;
     return this.values.pop();
   }
 
-  toArray() {
-    // oops, reverse() mutates!
-    // let's copy it first
-    // Option 1: slice
-    // return this.values.slice().reverse();
-
-    // Option 2: destructure
-    return [...this.values].reverse();
-  }
+ 
 }
 
 module.exports = PushPopStack;
