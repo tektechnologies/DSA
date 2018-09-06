38fc881a5b349274287968ba1af5c1cf14856a13
tektechnologies <info@tektechnologies.com>
2018-09-05 19:21:42 -0500

    add node and root constructors

---
+++ day_15_DSA/__tests__/tree-search-test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//create and initialize the node for the root and 
+//leafs that has a L and R.
+class Node{
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+//create and initialize the tree root to
+//then check that root for data on the left and right.
+class TreeSearch{
+  constructor(){
+    this.root = null;
+  }
+}
