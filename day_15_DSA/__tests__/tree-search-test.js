c9285d758f683152cda8b1deb1f23c5abed67d4c
tektechnologies <info@tektechnologies.com>
2018-09-05 20:39:59 -0500

    add test for null node and insert

--- day_15_DSA/__tests__/tree-search-test.js
+++ day_15_DSA/__tests__/tree-search-test.js
@@ -4,10 +4,17 @@
 
 const TreeSearch = require('../tree-search');
 
-describe('sort search trnpm ee', () => {
+describe('sort search tree', () => {
   it('has null data for root', () => {
     let sTree = new TreeSearch();
     expect(sTree.root).toBe(null);
   });
+
+  it('can insert data for root', () => {
+    let sTree = new TreeSearch();
+    sTree.insert(5);
+    expect(sTree.root).not.toBe(null);
+  });
+
 
 });
--- day_15_DSA/tree-search.js
+++ day_15_DSA/tree-search.js
@@ -16,14 +16,28 @@ class TreeSearch{
   constructor(){
     this.root = null;
   }
-}
+
+  //insert function to insert data into our tree nodes into leafs.
+  insert(data) {
+    let leaf;
+    if(data === null){
+      leaf = new Node(data);
+    } else {
+      leaf = data;
+      console.log(leaf);
+    }
+    if(!this.root){
+      this.root = leaf;
+      console.log(this.root);
+      return this;
+      
+    }
+    
+  }//close the insert function
 
 
 
 
 
-
-
-
-
+}//close tree search
 module.exports = TreeSearch;
