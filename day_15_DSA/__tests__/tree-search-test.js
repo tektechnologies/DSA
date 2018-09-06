17cae40be5f968af48432ace0400b39e1b38b8ba
tektechnologies <info@tektechnologies.com>
2018-09-05 19:39:30 -0500

    add null root test

--- day_15_DSA/__tests__/tree-search-test.js
+++ day_15_DSA/__tests__/tree-search-test.js
@@ -1,18 +1,13 @@
 'use strict';
 
-//create and initialize the node for the root and 
-//leafs that has a L and R.
-class Node{
-  constructor(data){
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
-}
-//create and initialize the tree root to
-//then check that root for data on the left and right.
-class TreeSearch{
-  constructor(){
-    this.root = null;
-  }
-}
+
+
+const TreeSearch = require('../tree-search');
+
+describe('sort search trnpm ee', () => {
+  it('has null data for root', () => {
+    let sTree = new TreeSearch();
+    expect(sTree.root).toBe(null);
+  });
+
+});
---
+++ day_15_DSA/tree-search.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+//create and initialize the node for the root and 
+//leafs that has a L and R.
+class Node{
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+//create and initialize the tree root to
+//then check that root for data on the left and right.
+class TreeSearch{
+  constructor(){
+    this.root = null;
+  }
+}
+
+
+
+
+
+
+
+
+
+module.exports = TreeSearch;
