87c8bb0c4e6587b7a236ceddb5946522bcb24bd9
tektechnologies <info@tektechnologies.com>
2018-09-08 17:31:41 -0500

    add a cat to queue and test

--- day_12_DSA/__tests__/fifo_animal_shelter_test.js
+++ day_12_DSA/__tests__/fifo_animal_shelter_test.js
@@ -7,9 +7,18 @@ describe('In and Out Animal Hostel', () => {
   it('can enqueue in expected order', () => {
     let cQue = new CutieQueue();
     expect(cQue.enqueue()).toBe(null);
-
-
   });
+
+  it('can add pet to adopt', () => {
+    let cQue = new CutieQueue();
+    let cat = {
+      type: 'tabby',
+      name: 'garrylasereyes',
+    };
+    cQue.enqueue(cat);
+    expect(cQue.head.pet.type).toBe('tabby');
+  });
+
 });
 
 
--- day_12_DSA/lib/fifo_animal_shelter.js
+++ day_12_DSA/lib/fifo_animal_shelter.js
@@ -1,22 +1,53 @@
 'use strict';
 
+
+//pet object constructor
+class Pet{
+  constructor(adoptPet){
+    this.pet = adoptPet;
+    this.next = null;
+    this.previous = null;  
+  }
+}
+
 class CutieQueue {
-  
+  //queue constructor
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
     this.catCount = 0;
     this.dogCount = 0;
-
   }
 
   enqueue(pet) {
     if(!pet){
       return null;
+    }//if no pet, adpotion is null. if new pet give the pet a head and tail.
+    let newPet = new Pet(pet);
+
+    if(!this.head){
+      this.head = newPet;
+      this.tail = newPet;
+    }//if we have a head and tail, lets add new pet to nodes tail.next
+    this.tail.next = newPet;
+    newPet.previous = this.tail;
+    this.tail = newPet;
+    this.lenght++;
+    //now that we have added to total pets, lets add by type
+    if(newPet.pet.type === 'cat'){
+      this.catCount++;
     }
-   
   }
+
+
+
+
+
+
+
+
+
 
   dequeue() {
     
