37c9d66d424708abad5434ff4461d4f8d3084f7c
tektechnologies <info@tektechnologies.com>
2018-09-24 13:31:21 -0500

    add new code

--- day_26_DSA/__tests__/breadth_first-test.js
+++ day_26_DSA/__tests__/breadth_first-test.js
@@ -1,21 +1,25 @@
 'use strict';
 
-var FizzBuzz = require('../../lib/fizzbuzztree');
+const Graph = require('../lib/breadth_first');
 
-describe('fizz', () => {
-  it('can enqueue in expected order', () => {
-   
+describe('Graph', () => {
+  it('can construct graph', () => {
+    let graph = new Graph();
+    graph.addNode('A');
+    graph.addNode('B');
+    graph.addNode('C');
+    graph.addNode('D');
+    graph.addNode('E'); 
+    //add edge.
+    graph.addEdge('A','B');
+    graph.addEdge('B','C');
+    graph.addEdge('B','D');
+    graph.addEdge('C','A');
+    graph.addEdge('C','E');  
+    let result = graph.bft('A');
+    expect(result).toEqual(['A', 'B', 'C', 'D', 'E']);
+
   });
 });
 
-describe('buzz', () => {
-  it('can  dequeue in expected order', () => {
-  
-  });
-});
 
-describe('fizzbuzz', () => {
-  it('can  dequeue in expected order', () => {
-  
-  });
-});
